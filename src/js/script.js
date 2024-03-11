70bf3e50503413214352b05d7ad0fea5b35111a7
Elderkhanov Alan <alan-1995@mail.ru>
2024-03-12 00:13:52 +0300

    add: team section

--- src/js/script.js
+++ src/js/script.js
@@ -33,3 +33,18 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const now = new Date();
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+  let some = i ** 3;
+}
+
+let end = new Date();
