37c4d759c349f10db65c9519db6ed3f116b369c2
Elderkhanov Alan <alan-1995@mail.ru>
2024-03-18 16:10:10 +0300

    add: media file

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // tabs
+
   const tabs = document.querySelectorAll(".reproduction__tabs-item");
   const tabsContent = document.querySelectorAll(".reproduction__wrapper");
   const tabsParent = document.querySelector(".reproduction__tabs-items");
@@ -32,19 +34,69 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  //modal
+
+  const modalTrigger = document.querySelectorAll("[data-modal]");
+  const modal = document.querySelector(".modal");
+  const modalCloseBtn = document.querySelector("[data-close]");
+
+  function modalOpen() {
+    modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+  }
+
+  function modalClose() {
+    modal.classList.toggle("show");
+    document.body.style.overflow = "";
+  }
+
+  modalTrigger.forEach((item) => {
+    item.addEventListener("click", modalOpen);
+  });
+
+  modalCloseBtn.addEventListener("click", modalClose);
+
+  // закрытие модалки при нажатии на экран
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modalClose();
+    }
+  });
+
+  // закрытие модалки на кнопку esc
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      modalClose();
+    }
+  });
+
+  // появление модалки через 5 секунд после захода на сайт
+  const modalTimerId = setTimeout(modalOpen, 5000);
+
+  // появление модалки при долистывании страницы до конца
+  function showModalByScroll() {
+    if (
+      window.scrollY + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      modalOpen();
+    }
+  }
+  window.addEventListener("scroll", showModalByScroll);
 });
 
-const now = new Date();
+// const now = new Date();
 
-console.log(now.getFullYear());
-console.log(now.getMonth());
-console.log(now.getDate());
-console.log(now.getDay());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
 
-let start = new Date();
+// let start = new Date();
 
-for (let i = 0; i < 100000; i++) {
-  let some = i ** 3;
-}
+// for (let i = 0; i < 100000; i++) {
+//   let some = i ** 3;
+// }
 
-let end = new Date();
+// let end = new Date();
